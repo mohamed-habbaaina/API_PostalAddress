903ba6a74be393f20bb66a68d3b8401fff644413
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-02-06 12:53:07 +0100

    fichier index, HTML & affichage

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Autocompletion</title>
+</head>
+<body>
+    <main>
+        <form action="#" id="form">
+            <label for="search">Villes Françaises</label>
+            <input type="number" name="search" id="search" placeholder="Entrer un Code Postale ...">
+            <div id="result"></div>
+            <div id="result2"></div>
+
+        </form>
+    </main>
+    <script src="./app.js"></script>
+</body>
+</html>
